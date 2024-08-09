6e40d45c43d9a7005874e3ea2009379975ad1fcd
AnroiDevel <anroidevel@gmail.com>
2024-08-09 17:15:09 +0300

    кузница

--- Assets/Game/Scripts/Models/ShopLot.cs
+++ Assets/Game/Scripts/Models/ShopLot.cs
@@ -9,22 +9,25 @@ namespace MultiTool
         [SerializeField] private Image _icon;
         [SerializeField] private Button _sellOne;
         [SerializeField] private Button _sellTen;
+        [SerializeField] private Text _priceOne;
+        [SerializeField] private Text _priceTen;
 
-        private ItemType _itemType;
         private DropItem _item;
         public event Action<DropItem, int> OnInventoryItemSell;
 
-        public ItemType ItemType => _itemType;
+        public DropItem Item => _item;
 
         public void Init(DropItem item, int count)
         {
             _item = item;
             _icon.sprite = item.Sprite;
-
+            _priceOne.text = item.SellCost.ToString();
+            _priceTen.text = (item.SellCost * 10).ToString();
             if(count > 0)
             {
                 _sellOne.interactable = count > 0;
                 _sellTen.interactable = count >= 10;
+                gameObject.SetActive(true);
             }
             else
             {
